64f6d27c34ada2503d8a3aab2e3440a2164d7901
khangt00 <vinhkhang324@gmail.com>
2022-03-08 20:31:09 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const setupContactRoutes = require("./app/routes/contact.routes");
+const { BadRequestError, errorHandler } = require("./app/errors");
 const app = express();
 
 app.use(cors());
@@ -10,4 +11,14 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application"});
 });
 setupContactRoutes(app);
+
+// handler 404 response
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resource not found"));
+});
+
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((error, req, res, next) => {
+    errorHandler.handleError(error, res);
+});
 module.exports = app;
