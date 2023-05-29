b0286516b20be887be5d88d7d4cb41e71ac0335c
Tran Khang <80498793+khangt00@users.noreply.github.com>
2023-05-29 10:55:35 +0700

    Update index.js

--- app/config/index.js
+++ app/config/index.js
@@ -3,7 +3,7 @@ const config = {
         port: process.env.PORT || 8080,
     },
     db: {
-        uri: process.env.MONGODB_URI || "mongodb+srv://khangt00:Lns7eKGh&u@cluster0.s76td.mongodb.net/CT449?retryWrites=true&w=majority"
+        uri: process.env.MONGODB_URI || "localhost"
     }
 };
 
